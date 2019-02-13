6179aa8123763fd33f06866ef8a1191f7c348b93
JaeKLee <jae.lee2@marist.edu>
2019-02-13 02:13:54 -0500

    Got linked list working

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -3,32 +3,57 @@
 # Opening file containing a list of words
 open_file = open("magicitems.txt", "r")
 
-# magicList = open_file.readline()
-# for x in open_file:
-#   print(x)
-
 # Creating the node to store data
 class node:
-  def createNode(self, value):
+  def __init__(self, value):
     self.value = value # Assigning object name to the value of " value "
     self.next = None # Because whatever is created is the last node'
   def printNode(self):
     print(self.value)
 
 # Linked List
-class linkedList:
+class singleLinkedList:
   # Initializing the head
-  def listHead(self):
-    self.head = None
+  def __init__(self, value):
+    self.head = node(value)
+  def addValueFront(self, data):
+    newNode = node(data)
+    newNode.next = self.head
+  def addValueEnd(self, data): 
+    nodePointer = self.head
+    while (nodePointer.next is not None):
+      nodePointer = nodePointer.next
+    newNode = node(data)
+    nodePointer.next = newNode
+  def printList(self):
+    nodePointer = self.head
+    while (nodePointer.next is not None):
+      print(nodePointer.value)
+      nodePointer = nodePointer.next
+    print(nodePointer.value)
 
+isFirst = True
 # Assigning node class to an assignment for easy access
-nodeCreation = node()
-linkList = linkedList()
-# headCounter = 0
+# # headCounter = 0
 for line in open_file:
-  nodeCreation.printNode()
-  linkList.head = nodeCreation.createNode(line)
+  if isFirst:
+    linkList = singleLinkedList(line)
+    isFirst = False
+  else: 
+    linkList.addValueEnd(line)
+linkList.printList()
+
+#   newNode.createNode(line)
+#   # newNode.printNode()
+#   while linkList.head is None:
+#     linkList.head = singleLinkedList()
+#     print("after while")
+#     # linkList.head = None
+#     print(linkList.head)
+    # newNode.printNode()
+    # linkList.head = nodeCreation.createNode(line)
   
 
   # linkList.listHead(headCounter+=1)
   # first.printNode()
+  
